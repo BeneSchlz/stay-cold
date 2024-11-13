1ec5162d32a5ac17c4f0f13397fc76908784c5ff
BeneSchlz <123456789+odin@bschulz735@gmail.com>
2024-11-13 08:51:42 +0100

    Added timer

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import time
 from datetime import datetime
-from blocker import start_block
+from blocker import *
 
 def schedule_block_started(block_name, start_hour = 23, start_minute = 0):
     while True:
         now = datetime.now()
         if now.hour == start_hour and now.minute == start_minute:
-            start_block(block_name)
-            time.sleep(60*60*24)
+            start_block("reddit week")
+            time.sleep(60 * 60 * 24)
         time.sleep(60)
 
 if __name__ == "__main__":
